91ae33e9f0bc2f53d9346105facebc77cbfaf19c
Molly Liao <mollyliao0803@gmail.com>
2024-01-18 17:08:48 -0800

    correct parsing row count

--- WordNetParser/WordNetParser.cpp
+++ WordNetParser/WordNetParser.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <sstream>
 #include <chrono>
+#include <windows.h>
+#include <mutex>
 #include "pugixml.hpp"
 
 using namespace std;
@@ -67,29 +69,28 @@ vector<string> getMembers(string list) {
 	// Remove unnecessary chars (oewn- and -n, -a, ...)
 	for (int i = 0; i < memberList.size(); i++) {
 		string member = memberList[i];
-		member = member.substr(5);
-		member = member.substr(0, member.length() - 2);
+		member = member.substr(5, member.length() - 7);
 		memberList[i] = member;
 	}
 
 	return memberList;
 }
 
+
 int main() {
 	xml_document doc;
 	unordered_map<string, xml_node> data;                             // map<synsetId, synset node>
 	unordered_map<string, string> sub;                                // map<senseId, senseSynset>
 
-	//const char* fileName = "environment-reference.xml";
-	//const char* fileName = "environment-complete.xml";
-	//const char* fileName = "gather-complete.xml";
-	//const char* fileName = "gather-n-and-v.xml";
-	//const char* fileName = "gathered-complete.xml";
-	const char* fileName = "5-word-test.xml";
+	int parsedCounter = 13;
+	int totalWordCount = 0;
+	int totalOutputRows = 0;
 
+	const char* fileName = "english-wordnet-2023.xml";
+
+	auto start = high_resolution_clock::now();
 
 	// Load XML file
-	auto start = high_resolution_clock::now();
 	xml_parse_result result = doc.load_file(fileName);
 	if (!result) {
 		cerr << "Error loading XML file: " << result.description() << endl;
@@ -97,64 +98,56 @@ int main() {
 	}
 
 	// Store synset id and synset node from Synset in map
-	int synsetCount = 0;
-	int relationCount = 0;
-
 	xml_node root = doc.child("LexicalResource").child("Lexicon");
-	for (xml_node synset = root.first_child().next_sibling("Synset"); synset; synset = synset.next_sibling("Synset")) {
-		synsetCount += 2;
+	for (xml_node synset = root.child("Synset"); synset; synset = synset.next_sibling("Synset")) {
+		parsedCounter += 2;
 		string synsetId = synset.attribute("id").value();
 		data[synsetId] = synset;
 
 		// Iterate over synsets and increment counter
-		for (xml_node synChild = synset.first_child(); synChild; synChild = synChild.next_sibling()) { relationCount++; }
+		for (xml_node synChild = synset.first_child(); synChild; synChild = synChild.next_sibling()) { parsedCounter++; }
 	}
 
 	// Store sense id and synset from LexicalEntry to map
-	int lexEntryCount = 6;
-	int senseCount = 0;
-	int lemmaCount = 0;
+	for (xml_node entry = root.child("LexicalEntry"); entry; entry = entry.next_sibling("LexicalEntry")) {
+		parsedCounter += 2;
 
-	for (xml_node entry = root.first_child(); entry; entry = entry.next_sibling("LexicalEntry")) {
-		lexEntryCount+=2;
-
-		for (xml_node sense = entry.first_child().next_sibling("Sense"); sense; sense = sense.next_sibling("Sense")) {
+		for (xml_node sense = entry.child("Sense"); sense; sense = sense.next_sibling("Sense")) {
 			string senseId = sense.attribute("id").value();
 			string senseSynset = sense.attribute("synset").value();
 			sub[senseId] = senseSynset;
 
 			// Iterate over Sense child nodes and increment row counter
 			if (sense.first_child()) {
-				senseCount+=2;
-				for (xml_node srelation = sense.first_child(); srelation; srelation = srelation.next_sibling()) { senseCount++; }
-			} else { senseCount++; }
+				parsedCounter += 2;
+				for (xml_node srelation = sense.first_child(); srelation; srelation = srelation.next_sibling()) { parsedCounter++; }
+			} else { parsedCounter++; }
 		}
 
 		// Iterate over Lemma child nodes and increment row counter
-		xml_node lemma = entry.first_child();
-		if (lemma.first_child()) {
-			lemmaCount+=2;
-			for (xml_node pronounciation = entry.first_child().first_child(); pronounciation; pronounciation = pronounciation.next_sibling()) { lemmaCount++; }
-		} else { lemmaCount++; }
+		xml_node lemma = entry.child("Lemma");
+		if (lemma. first_child()) {
+			parsedCounter += 2;
+			for (xml_node lchild = lemma.first_child(); lchild; lchild = lchild.next_sibling()) { parsedCounter++; }
+		}
+		else { parsedCounter++; }
+
+		// Iterate over Form and increment row counter
+		for (xml_node form = entry.child("Form"); form; form = form.next_sibling("Form")) { parsedCounter++; }
 	}
 
 	// Iterate over SyntacticBehaviour and increment row counter
-	int behaviourCount = 0;
-	for (xml_node sbehaviour = root.first_child().next_sibling("SyntacticBehaviour"); sbehaviour; sbehaviour = sbehaviour.next_sibling("SyntacticBehaviour")) { behaviourCount++; }
+	for (xml_node sbehaviour = root.child("SyntacticBehaviour"); sbehaviour; sbehaviour = sbehaviour.next_sibling("SyntacticBehaviour")) { parsedCounter++; }
 
 	// Open output CSV file
 	ofstream outputFile("results.csv");
 
 	// Iterate over LexicalEntries
-	int totalWordCount = 0;
-	int totalOutputRows = 0;
-
-	for (xml_node entry = root.first_child(); entry; entry = entry.next_sibling("LexicalEntry")) {
+	for (xml_node entry = root.child("LexicalEntry"); entry; entry = entry.next_sibling("LexicalEntry")) {
 		string wordA = entry.first_child().attribute("writtenForm").value();
 		totalWordCount++;
-		//cout << "WordA: " << wordA << endl;
 
-		for (xml_node sense = entry.first_child().next_sibling("Sense"); sense; sense = sense.next_sibling("Sense")) {
+		for (xml_node sense = entry.child("Sense"); sense; sense = sense.next_sibling("Sense")) {
 			string synsetId = sense.attribute("synset").value();
 
 			// Find entry's synset node
@@ -163,7 +156,7 @@ int main() {
 				xml_node synsetNode = synsetNodeIterator->second;
 
 				// Iterate over SynsetRelations
-				for (xml_node srelation = synsetNode.first_child().next_sibling("SynsetRelation"); srelation; srelation = srelation.next_sibling("SynsetRelation")) {
+				for (xml_node srelation = synsetNode.child("SynsetRelation"); srelation; srelation = srelation.next_sibling("SynsetRelation")) {
 					// Get target synset and relType/relationCode
 					string targetSyn = srelation.attribute("target").value();
 					string rtype = srelation.attribute("relType").value();
@@ -179,7 +172,6 @@ int main() {
 						vector<string> memberList = getMembers(members);
 						for (int i = 0; i < memberList.size(); i++) {
 							totalOutputRows++;
-							//cout << wordA << "," << relation << "," << memberList[i] << endl;
 							outputFile << wordA << "," << relation << "," << memberList[i] << endl;
 						}
 					}
@@ -189,10 +181,10 @@ int main() {
 	}
 
 	// Find sense relation target nodes
-	for (xml_node entry = root.first_child(); entry; entry = entry.next_sibling("LexicalEntry")) {
-		for (xml_node sense = entry.first_child().next_sibling("Sense"); sense; sense = sense.next_sibling("Sense")) {
-			string wordA = entry.first_child().attribute("writtenForm").value();
+	for (xml_node entry = root.child("LexicalEntry"); entry; entry = entry.next_sibling("LexicalEntry")) {
+		string wordA = entry.first_child().attribute("writtenForm").value();
 
+		for (xml_node sense = entry.child("Sense"); sense; sense = sense.next_sibling("Sense")) {
 			for (xml_node srelation = sense.first_child(); srelation; srelation = srelation.next_sibling("SenseRelation")) {
 				int relationCode = getRelationCode(srelation.attribute("relType").value());
 				string target = srelation.attribute("target").value();
@@ -211,7 +203,6 @@ int main() {
 						vector<string> memberList = getMembers(members);
 						for (int i = 0; i < memberList.size(); i++) {
 							totalOutputRows++;
-							//cout << wordA << "," << relationCode << "," << memberList[i] << endl;
 							outputFile << wordA << "," << relationCode << "," << memberList[i] << endl;
 						}
 					}
@@ -222,21 +213,17 @@ int main() {
 
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<milliseconds>(stop - start);
-	int totalRowsParsed = lexEntryCount + senseCount + lemmaCount + synsetCount + relationCount + behaviourCount;
 
-	cout << "Total rows parsed: " << totalRowsParsed << endl;
-	cout << "lexEntryCount: " << lexEntryCount << endl;
-	cout << "senseCount: " << senseCount << endl;
-	cout << "lemmaCount: " << lemmaCount << endl;
-	cout << "synsetCount: " << synsetCount << endl;
-	cout << "relationCount: " << relationCount << endl;
-	cout << "SyntacticBehaviour: " << behaviourCount << endl;
+	cout << "\nTotal rows parsed: " << parsedCounter << endl;
+	cout << "Total words: " << totalWordCount << endl;
+	cout << "Total output rows: " << totalOutputRows << endl;
+	cout << "Time spent: " << duration.count() << " ms" << endl;
 
-	outputFile << "\nTotal rows parsed: " << totalRowsParsed << endl;
+	outputFile << "\nTotal rows parsed: " << parsedCounter << endl;
 	outputFile << "Total words: " << totalWordCount << endl;
 	outputFile << "Total output rows: " << totalOutputRows << endl;
 	outputFile << "Time spent: " << duration.count() << " ms" << endl;
-
 	outputFile.close();
+
 	return 0;
 }
